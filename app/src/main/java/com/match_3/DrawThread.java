16625fd09b79c67ff37e8d40a58cce64a7a070ad
chris1502 <thomas-chris96@outlook.com>
2024-03-27 08:19:03 +0000

    Match 3 Game

---
+++ app/src/main/java/com/match_3/DrawThread.java
@@ -0,0 +1,58 @@
+package com.match_3;
+
+import android.graphics.Canvas;
+
+public class DrawThread extends Thread {
+
+    private GameView view;
+    public boolean running = false;
+    public static int fps;
+    public static int ups;
+
+    public DrawThread(GameView gameView) {
+        this.view = gameView;
+    }
+
+    public void setRunning(boolean run) {
+        running = run;
+    }
+
+    @Override
+    public void run() {
+        final int Max_FPS = 60;
+        final int MAX_UPS = 60;
+
+        final double fOPTIMAL_TIME = (double) 1000000000 / Max_FPS;
+        final double uOPTIMAL_TIME = (double) 1000000000 / MAX_UPS;
+
+        double uDeltaTime = 0, fDeltaTime = 0;
+        int frames = 0, updates = 0;
+        long startTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+
+//Game Loop starts from here...
+
+        while (running) {
+            long currentTime = System.nanoTime();
+        uDeltaTime +=(currentTime - startTime);
+        fDeltaTime +=(currentTime - startTime);
+        startTime = currentTime;
+
+        if (uDeltaTime >= uOPTIMAL_TIME){
+            //view.update;
+            updates ++;
+            uDeltaTime -= uOPTIMAL_TIME;
+        }
+        if (fDeltaTime >fOPTIMAL_TIME){
+            Canvas canvas = view.getHolder().lockCanvas(null);
+        if (canvas != null){
+            synchronized (view.getHolder()){
+                view.draw(canvas);
+            }
+            view.getHolder().unlockCanvasAndPost(canvas);
+        }
+        }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/match_3/GameView.java
@@ -0,0 +1,29 @@
+package com.match_3;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
