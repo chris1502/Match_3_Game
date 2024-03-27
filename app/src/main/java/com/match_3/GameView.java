34b36dda5c48628621de7dbdbc28671ed2453a44
chris1502 <thomas-chris96@outlook.com>
2024-03-27 18:06:38 +0000

    Match 3 Game jewels added

--- app/src/main/java/com/match_3/GameView.java
+++ app/src/main/java/com/match_3/GameView.java
@@ -20,15 +20,34 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private  DrawThread thread;
     private SpriteSheet spriteSheet;
     private Jewel jewel;
+    private  Jewel [][] board;
+    private int [][]level = {
+            {3, 2, 1, 2, 3, 1, 5, 4, 6},
+            {5, 3, 6, 2, 1, 5, 4, 5, 1},
+            {4, 5, 3 ,2, 3, 4, 4, 6, 4},
+            {5, 6, 1, 4, 2, 6, 2, 5, 6},
+            {6, 3, 3, 5, 3, 3, 2, 2, 4},
+            {2, 2, 4, 3, 4, 2, 4, 2, 3},
+            {1, 6, 4, 2, 6, 4, 3, 3, 5},
+            {4, 3, 5, 2, 1, 6, 2, 3, 5},
+            {5, 2, 1, 2, 3, 1, 5, 2, 1}
+    };
     public GameView(Context context) {
         super(context);
         getHolder().addCallback(this);
         thread = new DrawThread(this);
         spriteSheet = new SpriteSheet(getContext());
-        jewel = new Jewel((int)drawX,(int)drawY,0);
-        System.out.println("drawY" + drawY);
-    }
+        init();
 
+    }
+    public void init(){
+        board = new Jewel[level.length][level[0].length];
+        for (int i=0;i<level.length;i++){
+            for (int j=0;j<level[0].length;j++){
+                board[i][j] = new Jewel((int)drawX + (cellWidth*j),(int)drawY+(cellWidth*i),level[i][j]);
+            }
+        }
+    }
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
         thread.setRunning(true);
@@ -52,7 +71,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         canvas.drawColor(Color.WHITE);
         Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
         p.setColor(Color.BLACK);
-        canvas.drawBitmap(spriteSheet.topBG,0,0,null);
+        canvas.drawBitmap(spriteSheet.topBG,0,-cellWidth*2,null);
         canvas.drawBitmap(spriteSheet.bottomBG,0,drawY+cellWidth*9,null);
         canvas.drawBitmap(spriteSheet.bg_middle,0,drawY,null);
         for (int i = 0; i < 10;i++) {
@@ -62,7 +81,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
             }
         }
-        canvas.drawBitmap(spriteSheet.blue,jewel.poseX,jewel.poseY,null);
+        for (Jewel[] jewels : board){
+            for (Jewel jewel : jewels){
+                jewel.drawJewel(canvas,spriteSheet);
+            }
+        }
 
     }
 }
--- app/src/main/java/com/match_3/Jewel.java
+++ app/src/main/java/com/match_3/Jewel.java
@@ -1,14 +1,41 @@
 package com.match_3;
 
+import android.graphics.Canvas;
+
 public class Jewel {
+    public int poseX;
+    public int poseY;
+    public int color;
+
     public Jewel(int poseX, int poseY, int color) {
         this.poseX = poseX;
         this.poseY = poseY;
         this.color = color;
     }
 
-    public int poseX;
-    public int poseY;
-    public int color;
+    public void drawJewel(Canvas canvas, SpriteSheet spriteSheet){
+        switch (color){
+            case 1:
+                canvas.drawBitmap(spriteSheet.red,poseX,poseY,null);
+                break;
+            case 2:
+                canvas.drawBitmap(spriteSheet.blue,poseX,poseY,null);
+                break;
+            case 3:
+                canvas.drawBitmap(spriteSheet.yellow,poseX,poseY,null);
+                break;
+            case 4:
+                canvas.drawBitmap(spriteSheet.green,poseX,poseY,null);
+                break;
+            case 5:
+                canvas.drawBitmap(spriteSheet.purple,poseX,poseY,null);
+                break;
+            case 6:
+                canvas.drawBitmap(spriteSheet.orange,poseX,poseY,null);
+                break;
+
+        }
+    }
+
 
 }
