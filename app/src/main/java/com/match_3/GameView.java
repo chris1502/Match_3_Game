971ad43c04563b66175782fd3f0ac6e872388e30
chris1502 <thomas-chris96@outlook.com>
2024-03-28 10:35:44 +0000

    Match 3 Game removing matched jewels

--- app/src/main/java/com/match_3/GameView.java
+++ app/src/main/java/com/match_3/GameView.java
@@ -10,68 +10,148 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private  DrawThread thread;
+    private DrawThread thread;
     private SpriteSheet spriteSheet;
     private Jewel jewel;
-    private  Jewel [][] board;
+    private Jewel[][] board;
     private float oldX;
     private float oldY;
     private int poseI;
     private int poseJ;
-    public String direction;
+    private String direction;
     private int newPoseI;
     private int newPoseJ;
     private boolean move = false;
-    enum GameState{
-        swapping,checkSwapping,crushing,update,nothing
+    private ArrayList<ArrayList<Point>>search;
+
+    enum GameState {
+        swapping, checkSwapping, crushing, update, nothing
     }
+
     private int swapIndex = 8;
     public GameState gameState;
-    private int [][]level = {
+    private int [][] level = {
             {3, 2, 1, 2, 3, 1, 5, 4, 6},
-            {5, 3, 6, 2, 1, 5, 4, 5, 1},
-            {4, 5, 3 ,2, 3, 4, 4, 6, 4},
+            {5, 3, 6, 3, 1, 5, 4, 5, 1},
+            {4, 5, 3, 2, 3, 4, 4, 6, 4},
             {5, 6, 1, 4, 2, 6, 2, 5, 6},
-            {6, 3, 3, 5, 3, 3, 2, 2, 4},
+            {6, 1, 3, 5, 3, 6, 2, 2, 4},
             {2, 2, 4, 3, 4, 2, 4, 2, 3},
             {1, 6, 4, 2, 6, 4, 3, 3, 5},
-            {4, 3, 5, 2, 1, 6, 2, 3, 5},
+            {4, 3, 2, 1, 1, 6, 2, 3, 5},
             {5, 2, 1, 2, 3, 1, 5, 2, 1}
     };
+
     public GameView(Context context) {
         super(context);
         getHolder().addCallback(this);
         thread = new DrawThread(this);
         spriteSheet = new SpriteSheet(getContext());
         gameState = GameState.nothing;
+        search = new ArrayList<>();
         init();
 
     }
-    public void init(){
+
+    public void init() {
         board = new Jewel[level.length][level[0].length];
-        for (int i=0;i<level.length;i++){
-            for (int j=0;j<level[0].length;j++){
-                board[i][j] = new Jewel((int)drawX + (cellWidth*j),(int)drawY+(cellWidth*i),level[i][j]);
+        for (int i = 0; i < level.length; i++) {
+            for (int j = 0; j < level[0].length; j++) {
+                board[i][j] = new Jewel((int) drawX + (cellWidth*j), (int) drawY + (cellWidth*i), level[i][j]);
             }
         }
     }
 
-    public void update(){
+    public void update() {
         switch (gameState){
             case swapping:
                 swap();
                 break;
+            case checkSwapping:
+                fillCrushing();
+                if (search.isEmpty()){
+                    swap();
+                }else {gameState = GameState.crushing;
+                }// here we need to check the crushing
+                break;
+            case crushing:
+                for (int i=0;i<search.size();i++){
+                    for (int j=0;j<search.get(i).size();j++){
+                        board[search.get(i).get(j).x][search.get(i).get(j).y].color = 0;
+                    }
+                    search.remove(i);
+                    i--;
+                }
+
+                if (search.isEmpty()) {
+                    gameState = GameState.nothing;
+                }
+                break;
+            case update:
+                break;
         }
     }
-private void swap(){
+
+    private void fillCrushing(){
+        search.clear();
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board[0].length;j++){
+                if (board[i][j].color > 0){
+                    if (j<board.length-2 && board[i][j].color == board[i][j+1].color && board[i][j+1].color == board[i][j+2].color){
+                        search.add(new ArrayList<>());
+                        search.get(search.size() - 1).add(new Point(i,j));
+                        search.get(search.size() - 1).add(new Point(i,j + 1));
+                        search.get(search.size() - 1).add(new Point(i,j + 2));
+                        j = j+2;
+                    }
+                }
+            }
+        }
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board[0].length;j++){
+                if (board[i][j].color>0){
+                    if (i<board.length-2 && board[i][j].color == board[i+1][j].color && board[i+1][j].color == board[i+2][j].color){
+                        search.add(new ArrayList<>());
+                        search.get(search.size() - 1).add(new Point(i,j));
+                        search.get(search.size() - 1).add(new Point(i+1,j));
+                        search.get(search.size() - 1).add(new Point(i+2,j));
+                        i = i+2;
+                    }
+                }
+            }
+        }
+        for (int i=0;i<search.size();i++){
+            if (!allowCrushing(search.get(i))){
+                search.remove(i);
+                i--;
+            }
+        }
+    }
+
+    private boolean allowCrushing(ArrayList<Point> points) {
+        boolean allow = true;
+        for (int i=0;i<points.size();i++){
+            if (points.get(i).x < board.length-1){
+                if(board[points.get(i).x+1][points.get(i).y].color ==0) allow=false;
+            }
+        }
+        return  allow;
+    }
+
+
+    private void swap(){
         if (swapIndex > 0) {
             switch (direction) {
                 case "right":
@@ -102,9 +182,11 @@ private void swap(){
             board[poseI][poseJ].poseY = (int) (poseI * cellWidth + drawY);
             board[newPoseI][newPoseJ].poseX = (int) (newPoseJ * cellWidth +drawX);
             board[newPoseI][newPoseJ].poseY = (int) (newPoseI * cellWidth +drawY);
+            swapIndex = 8;
+            //after we finish swapping we go to check swapping, and then after checking we crushing
             if (gameState == GameState.swapping){
-                gameState = GameState.nothing;
-            }
+                gameState = GameState.checkSwapping;
+            }else gameState = GameState.nothing;
         }
 }
     @Override
@@ -140,6 +222,7 @@ private void swap(){
                 oldY = event.getY();
                 poseI = (int) (oldY - drawY) / cellWidth;
                 poseJ = (int) (oldX - drawX) / cellWidth;
+
                 move = true;
                 break;
             case MotionEvent.ACTION_MOVE:
@@ -148,8 +231,7 @@ private void swap(){
                     float newY = event.getY();
                     float deltaX = Math.abs(newX - oldX);
                     float deltaY = Math.abs(newY - oldY);
-                    if (move && (deltaX > 5 || deltaY > 5 )){
-                        //checking how many pixels moved, if we move more than 3o pixels we start checking in which direction
+                    if (move && (deltaX > 30 || deltaY > 30 )){ //checking how many pixels moved, if we move more than 3o pixels we start checking in which direction
                         move = false;
                         if (Math.abs( oldX - newX) > Math.abs(oldY - newY)){
                             //if the first x touch  - the end of the x touch is bigger
